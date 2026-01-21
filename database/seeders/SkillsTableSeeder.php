d204ea11e9f9b579c23e58a1126661949792a099
Lorenz Ciocon <lorenzciocon@gmail.com>
2026-01-21 12:17:47 +0800

    Database Migration and Seeding

---
+++ database/seeders/SkillsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SkillsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('skills')->insert([
+            ['skill'=>'HTML', 'level'=>'Advanced'], 
+            ['skill'=>'CSS', 'level'=>'Advanced'], 
+            ['skill'=>'JavaScript', 'level'=>'Intermediate'], 
+            ['skill'=>'PHP', 'level'=>'Intermediate'], 
+            ['skill'=>'Laravel', 'level'=>'Beginner']
+        ]);
+    }
+}

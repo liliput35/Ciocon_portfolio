1166eb86170ab6a09b7a6a2ab2dec294b57a3a90
Lorenz Ciocon <lorenzciocon@gmail.com>
2026-01-28 12:22:03 +0800

    Lab Activity 3: Database and Eloquent ORM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,27 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>Welcome to My Portfolio</h1>
-<p>This is my personal website</p>
+
+<div class="container mt-4">
+    <h2>My Skills</h2>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Skill</th>
+                <th>Level</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($skills as $skill)
+                <tr>
+                    <td>{{ $skill -> skill}}</td>
+                    <td>{{ $skill -> level}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
